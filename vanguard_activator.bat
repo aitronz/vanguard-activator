09b48a238f003b4ac39a35dc53218533170a7792
aitronssesin <74368135+aitronssesin@users.noreply.github.com>
2022-02-19 02:06:05 +0100

    Create vanguard_activator.bat

---
+++ vanguard_activator.bat
@@ -0,0 +1,43 @@
+@echo off
+TITLE Vanguard Activator
+:: BatchGotAdmin
+:-------------------------------------
+REM  --> Check for permissions
+    IF "%PROCESSOR_ARCHITECTURE%" EQU "amd64" (
+>nul 2>&1 "%SYSTEMROOT%\SysWOW64\cacls.exe" "%SYSTEMROOT%\SysWOW64\config\system"
+) ELSE (
+>nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"
+)
+
+REM --> If error flag set, we do not have admin.
+if '%errorlevel%' NEQ '0' (
+    echo Requesting administrative privileges...
+    goto UACPrompt
+) else ( goto gotAdmin )
+
+:UACPrompt
+    echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
+    set params= %*
+    echo UAC.ShellExecute "cmd.exe", "/c ""%~s0"" %params:"=""%", "", "runas", 1 >> "%temp%\getadmin.vbs"
+
+    "%temp%\getadmin.vbs"
+    del "%temp%\getadmin.vbs"
+    exit /B
+
+:gotAdmin
+    pushd "%CD%"
+    CD /D "%~dp0"
+
+timeout /t 1
+
+sc config vgc start=delayed-auto
+
+timeout /t 1
+
+net start vgc
+
+timeout /t 1
+
+start /d "C:\Program Files\Riot Vanguard\" vgtray.exe
+
+timeout /t 1
